fce41ba17b80ee3e16f015b11f9ec30e8ab646bc
Diogo.Lopes <diogo.lopes@koerber.digital>
2021-04-17 22:30:58 +0100

    Change query for table creation

--- query.sql
+++ query.sql
@@ -1,10 +1,28 @@
-DROP TABLE IF EXISTS USERS;
-
 CREATE TABLE IF NOT EXISTS
-USERS
+ALUNOS_MODSI
 (
-    ID int PRIMARY KEY NOT NULL,
-    Student_Number varchar(255) UNIQUE NOT NULL,
-    Course varchar(50) DEFAULT NULL
+    mec_aluno int PRIMARY KEY NOT NULL,
+    nome varchar(255) UNIQUE NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS
+DIRETOR
+(
+    sigla varchar(5) PRIMARY KEY UNIQUE NOT NULL,
+    nome_diretor varchar(50) UNIQUE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS
+PROJETOS
+(
+    nome_projeto varchar(50) UNIQUE NOT NULL,
+    status_project varchar(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS
+INFO_PROJETOS
+(
+    sigla_resp varchar(5) UNIQUE NOT NULL,
+    departamento varchar(50),
+    FOREIGN KEY (sigla_resp) REFERENCES DIRETOR (sigla)
+);
