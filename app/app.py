e03a76092e5a5f10a37f20f5f20b2eb9d3cbe705
Diogo.Lopes <diogo.lopes@koerber.digital>
2021-04-20 22:11:44 +0100

    Add another page

--- app/app.py
+++ app/app.py
@@ -4,22 +4,32 @@ import psycopg2
 from flask import Flask
 import database_conn as db
 
-# app = Flask(__name__)
+app = Flask(__name__)
 
 random_list = [1170503, 'Diogo']
 
 
-# @app.route('/')
-# def main_page():
+####################
+##      TODO      ##
+####################
 
-#     text = '<a href="%s">Yolo bitches</a>'
-#     return text % connect()
+# 1. 1º login guarda na DB (Registo)
+# 2. Procura na base de dados (SQL query)
+# 3. Se existir informação na DB - login
+
+@app.route('/')
+def main_page():
+
+    text = '<a href="%s">Yolo bitches</a>'
+    return text % connect()
 
 
+@app.route('/login')
 def connect():
     db.connection_db(random_list)
 
+    return "Data inserted"
+
 
 if __name__ == "__main__":
-    # app.run(debug=True, port=65200)
-    connect()
+    app.run(debug=True, host='0.0.0.0', port=65200)
