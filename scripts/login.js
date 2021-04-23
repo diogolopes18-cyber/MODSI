3eb99c427c71eab14a99c7502b30e67943e45570
Diogo.Lopes <diogo.lopes@koerber.digital>
2021-04-23 23:48:55 +0100

    Add scripts folder for JS validation forms

---
+++ scripts/login.js
@@ -0,0 +1,8 @@
+function validateForm() {
+    var name = document.forms["myForm"]["fname"].value;
+    var password;
+    if (x == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+}
