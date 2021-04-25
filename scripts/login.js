904a05929ba9e0f3fe8a2655a89148b67e266631
Diogo.Lopes <diogo.lopes@koerber.digital>
2021-04-25 16:17:38 +0100

    Add form verification script

--- scripts/login.js
+++ scripts/login.js
@@ -1,7 +1,7 @@
 function validateForm() {
-    var name = document.forms["myForm"]["fname"].value;
-    var password;
-    if (x == "") {
+    var name = form.getElementById('username_login');
+    var password = form.getElementById('password_login');
+    if (name === "" || password === "") {
         alert("Name must be filled out");
         return false;
     }
