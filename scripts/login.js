8cf88ced4e381b1718078dba88e4d4dd05f5ef80
Diogo.Lopes <diogo.lopes@koerber.digital>
2021-05-03 14:53:18 +0100

    Make sure passwords match on JS script

--- scripts/login.js
+++ scripts/login.js
@@ -1,8 +1,16 @@
 function validateForm() {
     var name = form.getElementById('username_login');
     var password = form.getElementById('password_login');
+    var password_confirm = form.getElementById('password_login_confirm')
+
     if (name === "" || password === "") {
         alert("Name must be filled out");
         return false;
     }
+
+    if (password != password_confirm) {
+        alert("Passwords must match");
+        return false;
+    }
+
 }
