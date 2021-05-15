6909bcc18b8dfa2a19d7d20334902f5d2c302b72
Diogo.Lopes <diogo.lopes@koerber.digital>
2021-05-15 22:44:50 +0100

    Add upload file method

--- app.py
+++ app.py
@@ -2,11 +2,19 @@
 
 import os
 import json
-import psycopg2
-from flask import Flask, render_template, flash, request, redirect, url_for
+from flask import Flask, render_template, flash, request, redirect, url_for, send_from_directory
 import database_conn as db
+from uuid import uuid4
+from werkzeug.utils import secure_filename
+#from student import student
 
 app = Flask(__name__)
+
+######################
+##  App blueprints  ##
+######################
+# app.register_blueprint(student)
+
 error = None
 authorize = 0
 
@@ -28,7 +36,7 @@ def login():
             error = 'Invalid Credentials. Please try again.'
         else:
             db.connection_db(random_list, query="search")
-            return redirect(url_for('aluno'))
+            return redirect(url_for('student.submit_paper'))
 
     return render_template("login.html", error=error)
 
@@ -68,12 +76,44 @@ def forgot_password():
     return render_template("forgot_password.html", error=error)
 
 
-@app.route('/aluno', methods=['GET', 'POST'])
-def submit_paper():
-    # uploaded_file = request.files['input_name']
-    # assert len(uploaded_file) > 0, "File size must be greater than zero"
+UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/uploads/'
+DOWNLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/downloads/'
+ALLOWED_EXTENSIONS = {'pdf', 'txt', 'png'}
+DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
-    return render_template("aluno.html")
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['DOWNLOAD_FOLDER'] = DOWNLOAD_FOLDER
+# limit upload size upto 8mb
+app.config['MAX_CONTENT_LENGTH'] = 8 * 1024 * 1024
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/test', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            print('No file attached in request')
+            return redirect(request.url)
+        file = request.files['file']
+
+        if file.filename == '':
+            print('No file selected')
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            #process_file(os.path.join(app.config['UPLOAD_FOLDER'], filename), filename)
+            return redirect(url_for('uploaded_file', filename=filename))
+    return render_template('upload.html')
+
+
+@app.route('/uploads/<filename>', methods=['GET'])
+def uploaded_file(filename):
+    return send_from_directory(app.config['DOWNLOAD_FOLDER'], filename, as_attachment=True)
 
 
 if __name__ == "__main__":
