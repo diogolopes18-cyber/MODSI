ade7e0bb4922570836a1e944e1739ac70355bb13
Diogo.Lopes <diogo.lopes@koerber.digital>
2021-05-22 13:41:44 +0100

    Return to old connection implementation now that tests are completed

--- database_conn.py
+++ database_conn.py
@@ -9,7 +9,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 DATABASE_URL = os.environ['DATABASE_URL']
-hello = [1, 2, 3]
 
 
 def connection_db(data_for_db, *args, **kwargs):
@@ -24,28 +23,26 @@ def connection_db(data_for_db, *args, **kwargs):
         cursor = connection.cursor()
         print(connection.get_dsn_parameters(), "\n")
 
-        # #####################
-        # # Data insertion
-        # #####################
-        # if(query == "insert"):
-        #     insert_query = "INSERT INTO alunos_modsi (mec_aluno, pass, mail) VALUES %s"
-        #     assert len(data_for_db) != 0, "No data to insert"
-        #     cursor.execute(
-        #         insert_query, [(data_for_db[0], data_for_db[1]), data_for_db[2]])
+        #####################
+        # Data insertion
+        #####################
+        if(query == "insert"):
+            insert_query = "INSERT INTO alunos_modsi (mec_aluno, pass, mail) VALUES %s"
+            assert len(data_for_db) != 0, "No data to insert"
+            cursor.execute(
+                insert_query, [(data_for_db[0], data_for_db[1]), data_for_db[2]])
 
-        # elif(query == "search"):
-        #     # Could also be "IF EXISTS(SELECT mec_aluno FROM alunos_modsi WHERE mec_aluno=%s) THEN
-        #     #  raise notice 'yes'
-        #     # END IF"
-        #     search_query = "SELECT EXISTS(SELECT mec_aluno FROM alunos_modsi WHERE mec_aluno=%s)"
-        #     cursor.execute(search_query, [data_for_db[0]])
+        elif(query == "search"):
+            # Could also be "IF EXISTS(SELECT mec_aluno FROM alunos_modsi WHERE mec_aluno=%s) THEN
+            #  raise notice 'yes'
+            # END IF"
+            search_query = "SELECT EXISTS(SELECT mec_aluno FROM alunos_modsi WHERE mec_aluno=%s)"
+            cursor.execute(search_query, [data_for_db[0]])
 
-        # elif(query == "update"):
-        #     update_query = "UPDATE alunos_modsi SET pass=%s WHERE username=%s"
-        #     cursor.execute(update_query, data_for_db[1], data_for_db[0])
+        elif(query == "update"):
+            update_query = "UPDATE alunos_modsi SET pass=%s WHERE username=%s"
+            cursor.execute(update_query, data_for_db[1], data_for_db[0])
 
-        test = cursor.execute("SELECT * FROM public.alunos_modsi")
-        print(test)
         #####################
         # Data commit
         #####################
@@ -57,7 +54,3 @@ def connection_db(data_for_db, *args, **kwargs):
         connection.close()
 
     print("Closing...")
-
-
-if __name__ == "__main__":
-    connection_db(hello)
