d41de79b35070e6da22d3aa6f7aa52bf3962873a
Diogo.Lopes <diogo.lopes@koerber.digital>
2021-06-03 16:45:16 +0100

    Add orientador table

--- db/create_db.sql
+++ db/create_db.sql
@@ -15,16 +15,20 @@ DIRETOR
 );
 
 CREATE TABLE IF NOT EXISTS
-PROJETOS
+ORIENTADOR
 (
-    nome_projeto varchar(50) UNIQUE,
-    status_project varchar(50)
+    sigla varchar(5) PRIMARY KEY UNIQUE NOT NULL,
+    pass varchar(50) NOT NULL,
+    email varchar(50) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS
-INFO_PROJETOS
+PROJETOS
 (
-    sigla_resp varchar(5) UNIQUE NOT NULL,
-    departamento varchar(50),
-    FOREIGN KEY (sigla_resp) REFERENCES DIRETOR (sigla)
+    nome_projeto varchar(50) UNIQUE,
+    status_project varchar(50),
+    student_id int UNIQUE NOT NULL,
+    sigla_orientador varchar(5) UNIQUE NOT NULL,
+    FOREIGN KEY (student_id) REFERENCES ALUNOS_MODSI (mec_aluno),
+    FOREIGN KEY (sigla_orientador) REFERENCES ORIENTADOR (sigla)
 );
