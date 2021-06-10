6dff77d798f9d70068d7fd52758a13a8b0ed2c8d
Diogo.Lopes <diogo.lopes@koerber.digital>
2021-06-10 17:05:42 +0100

    Add route for conselour to submit grades for a project

--- user/orientador_page.py
+++ user/orientador_page.py
@@ -46,31 +46,19 @@ def available_projects():
     return render_template("available_projects.html", data=available)
 
 
-#############################
-# 1. Route para approve
-#############################
-# @diretor.route('/diretor/approve', methods=['GET', 'POST'])
-# def approved_projects(prof_name):
-#     if(request.method == 'GET'):
-#         assert len(prof_name) > 0, "No name provided"
-#         prof_exist = db.connection_db(prof_name, query="search", tablename="diretor")
-#         if(prof_exist == 1):
-#             if(request.form['approve_button'] == 'approve'):
-#                 # Update column status_project of table projetos with approved
-#                 status = "approved"
-#                 db.connection_db(status, query="update", tablename="diretor")
-#         else:
-#             pass
-#     return render_template("professor_aprove.html")
-# @diretor.route('/diretor/proposals', methods=['GET', 'POST'])
-# def proposals():
-#     # Presents proposed projects by professor
-#     if(request.method == 'POST'):
-#         if(request.form['propose_project'] == "true"):
-#             # Insert text box
-#         else:
-#             pass
-#     elif(request.method == 'GET'):
-#         # Fetch from DB and present to HTML page
-#         projects_from_professor = db.connection_db()
-#         return render_template("diretor_proposals.html", data=projects_from_professor)
+@orientador.route('/orientador/submit_grade', methods=['GET', 'POST'])
+def submit_grade():
+
+    if(request.method == 'POST'):
+
+        # Submit final grade
+        grade = {
+            "student": request.form['student'],
+            "project_name": request.form['project'],
+            "grade": request.form['note']
+        }
+
+        # Insert data into DB
+        db.connection_db(query="insert", tablename="grades")
+
+    return render_template("final_grade.html")
