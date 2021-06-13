0eb7dc75d9f57cff9d9b0f2bc7e08f3ef731e4a9
Diogo.Lopes <diogo.lopes@koerber.digital>
2021-06-13 19:24:34 +0100

    Add optional argument to function

--- user/public_projects.py
+++ user/public_projects.py
@@ -7,11 +7,11 @@ import database_conn as db
 public = Blueprint('public', __name__)
 
 
-@public.route('/public', methods=['GET'])
+@public.route('/public', methods=['GET', 'POST'])
 def public_projects():
 
     if(request.method == 'GET'):
         # Request approved projects
-        approved_projects = db.connection_db(query="select", tablename="projetos")
+        approved_projects = db.connection_db(query="select", tablename="projetos", public="true")
 
     return render_template("publico.html", data=approved_projects)
